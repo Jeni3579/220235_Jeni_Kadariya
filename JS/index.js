9c6abe71c5f5c3ef008c7005ce24ff325981df92
jeni3579 <kadariyajenny888@gmail.com>
2023-05-15 10:28:18 +0545

    java script

---
+++ JS/index.js
@@ -0,0 +1,20 @@
+function myfunction()  { 
+    console.log("Button Clicked");
+}
+
+Let obtained_mark = 66;
+if(obtained_mark >= 10) {
+    console.log("Distinction");
+}    
+else if(obtained_mark >= 60 && obtained_mark < 70){
+    console.log("First Class");
+}
+else if(obtained_mark >= 50 && obtained_mark < 60){
+    console.log("Second Class");
+}
+else if(obtained_mark >= 40 && obtained_mark < 50){
+
+}
+else{
+    console.log("Fail");
+}
---
+++ JS/switch.js
@@ -0,0 +1,16 @@
+let obtained_mark = 66;
+if(obtained_mark >= 10) {
+    console.log("Distinction");
+}    
+else if(obtained_mark >= 60 && obtained_mark < 70){
+    console.log("First Class");
+}
+else if(obtained_mark >= 50 && obtained_mark < 60){
+    console.log("Second Class");
+}
+else if(obtained_mark >= 40 && obtained_mark < 50){
+
+}
+else{
+    console.log("Fail");
+}
